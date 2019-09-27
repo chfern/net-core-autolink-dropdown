9418338b38cf2c2cea91d9e96a0b70df8d9f2e6d
Fernando Christyanto <christyantofernando@gmail.com>
2019-09-27 13:45:08 +0700

    Add base dropdown interfaces and dropdown registry

---
+++ NetCoreAutoLinkDropdown/AutoLinkDropdown/AutoLinkDropdownRegistry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreAutoLinkDropdown.AutoLinkDropdown
+{
+	public class AutoLinkDropdownRegistry
+	{
+		public static AutoLinkDropdownRegistry Registry { get; set; }
+		public Dictionary<string, List<DropdownItem>> DropdownItemsDict { get; private set; } = new Dictionary<string, List<DropdownItem>>();
+
+		public static AutoLinkDropdownRegistry Get()
+		{
+			if (Registry == null) Registry = new AutoLinkDropdownRegistry();
+			return Registry;
+		}
+
+		public void AddToDict(string ProviderDdlId, List<DropdownItem> Data)
+		{
+			if (DropdownItemsDict.ContainsKey(ProviderDdlId))
+			{
+				DropdownItemsDict.Remove(ProviderDdlId);
+			}
+			DropdownItemsDict.Add(ProviderDdlId, Data);
+		}
+
+		public List<DropdownItem> GetFromDict(string ProviderDdlId)
+		{
+			List<DropdownItem> dropdowns = new List<DropdownItem>();
+			DropdownItemsDict.TryGetValue(ProviderDdlId, out dropdowns);
+			return dropdowns;
+		}
+	}
+}
---
+++ NetCoreAutoLinkDropdown/AutoLinkDropdown/DropdownItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreAutoLinkDropdown.AutoLinkDropdown
+{
+	public interface DropdownItem
+	{
+		string DropdownValue();
+		string DropdownText();
+		bool DropdownItemEnabled();
+	}
+}
